7fe0a5dd01643fd0bc8158cb09ea540a1b86bbeb
Dongha Yoon <cs.donghayoon@gmail.com>
2020-08-26 03:35:55 +0900

    0826

--- source/cont_sum.cpp
+++ source/cont_sum.cpp
@@ -3,6 +3,7 @@
 https://www.acmicpc.net/problem/1912
 
 #review
+https://ssyoon.tistory.com/18
 
 #reference
 
@@ -12,34 +13,20 @@ https://www.acmicpc.net/problem/1912
 
 using namespace std;
 
-vector<int> sumSet;
 vector<int> max_sum;
 
 int main(){
     int N;
     cin >> N;
-    int pos=0;
-    for(int i=0;i<N;i++){
+    
+    max_sum.resize(N+1);
+    int Answer=max_sum[0]=-1000;
+
+    for(int i=1;i<N+1;i++){
         int input;
         scanf("%d",&input);
-        if(input <= 0){
-            if(pos!=0)
-                sumSet.push_back(pos);
-            sumSet.push_back(input);
-            pos=0;
-        }
-        else
-            pos+=input;
-    }
-    if(pos!=0)  sumSet.push_back(pos);
-    
-    max_sum.resize(sumSet.size());
-    int Answer=max_sum[0]=sumSet[0];
-
-    for(int i=1;i<sumSet.size();i++){
-        max_sum[i] = max(max_sum[i-1]+sumSet[i],sumSet[i]);
+        max_sum[i] = max(max_sum[i-1]+input,input);
         Answer = max(max_sum[i],Answer);
     }
-    // cout << "Size : " << max_sum.size() << endl;
     cout << Answer << endl;
 }
