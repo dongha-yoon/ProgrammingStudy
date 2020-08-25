1b936a564cbef9c8f762436fa4e54dc9e152a337
Dongha Yoon <cs.donghayoon@gmail.com>
2020-08-25 22:44:47 +0900

    0825

--- source/sugar.cpp
+++ source/sugar.cpp
@@ -1,5 +1,6 @@
 /*
 # problem
+https://www.acmicpc.net/problem/2839
 
 #review
 
@@ -7,9 +8,32 @@
 
 */
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+#define MAX 10000
+
+int sugar[5001];
+
+void init(){
+    for(int i=0;i<=5000;i++)
+        sugar[i] = MAX;
+    sugar[3] = sugar[5] = 1;
+}
+
 int main(){
+    init();
     
+    int N;
+    cin >> N;
+    for(int i=0;i<N;i++){
+            if(sugar[i]==-1)
+                continue;
+            else{
+                sugar[i+3] = min(sugar[i+3],sugar[i] + 1);
+                sugar[i+5] = min(sugar[i+5],sugar[i] + 1);
+            }
+        }
+    cout << (sugar[N]==MAX? -1:sugar[N]) << endl;
 }
