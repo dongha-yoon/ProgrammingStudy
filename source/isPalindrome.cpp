18134792ec43cfbb764c8e596aed0af81eaf90b8
Dongha Yoon <cs.donghayoon@gmail.com>
2020-09-02 21:25:19 +0900

    0902

--- source/isPalindrome.cpp
+++ source/isPalindrome.cpp
@@ -14,19 +14,17 @@ https://www.acmicpc.net/problem/10942
 using namespace std;
 
 
-typedef pair<int,int> pii;
-
-map<pii,int> dp;
+int dp[2001][2001];
 int arr[2001];
 
-int solve(int S,int E){
-    if(S==E) return dp[pii(S,E)]=1;
-    if(E-S == 1) return dp[pii(S,E)]=(arr[S]==arr[E])? 1:0;
-    if(dp.find(pii(S,E))!=dp.end()) return dp[pii(S,E)];
+// int solve(int S,int E){
+//     if(S==E) return dp[S][E]=1;
+//     if(E-S == 1) return dp[S][E]=(arr[S]==arr[E])? 1:-1;
+//     if(dp[S][E]!=0) return dp[S][E];
     
-    if(arr[S]==arr[E]) return dp[pii(S,E)]=solve(S+1,E-1);
-    else return dp[pii(S,E)]= 0;
-}
+//     if(arr[S]==arr[E]) return dp[S][E]=solve(S+1,E-1);
+//     else return dp[S][E]= -1;
+// }
 
 
 int main(){
@@ -36,13 +34,25 @@ int main(){
 
     for(int i=1;i<=N;i++){
         scanf("%d",&arr[i]);
+        dp[i][i]=1;
+        for(int j=i-1;j>0;j--){
+            if(arr[i]==arr[j]){
+                if (dp[i-1][j+1]==1 || j+2>i ) dp[i][j]=1;
+            }
+        }
     }
+
     int M;
     cin >> M;
     for(int i=0;i<M;i++){
         int input1,input2;
         scanf("%d %d",&input1,&input2);
-    
-        cout << solve(input1,input2) << endl;
+        // solve(input1,input2);
+        // if(input1 > input2) continue;
+        printf("%d\n",dp[input2][input1]);
     }
+
+    // for(auto i=dp.begin();i!=dp.end();i++){
+    //     cout << i->first.first << " -> " << i->first.second << " : " << i->second <<endl;
+    // }
 }
