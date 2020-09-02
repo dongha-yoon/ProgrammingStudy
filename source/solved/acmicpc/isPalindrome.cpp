71cdad8f62d552b26b12f020dfbd9d6f8090e6ce
Dongha Yoon <cs.donghayoon@gmail.com>
2020-09-02 22:38:02 +0900

    0902

--- source/solved/acmicpc/isPalindrome.cpp
+++ source/solved/acmicpc/isPalindrome.cpp
@@ -3,6 +3,7 @@
 https://www.acmicpc.net/problem/10942
 
 #review
+https://ssyoon.tistory.com/19
 
 #reference
 
@@ -16,16 +17,6 @@ using namespace std;
 
 int dp[2001][2001];
 int arr[2001];
-
-// int solve(int S,int E){
-//     if(S==E) return dp[S][E]=1;
-//     if(E-S == 1) return dp[S][E]=(arr[S]==arr[E])? 1:-1;
-//     if(dp[S][E]!=0) return dp[S][E];
-    
-//     if(arr[S]==arr[E]) return dp[S][E]=solve(S+1,E-1);
-//     else return dp[S][E]= -1;
-// }
-
 
 int main(){
 
@@ -41,18 +32,11 @@ int main(){
             }
         }
     }
-
     int M;
     cin >> M;
     for(int i=0;i<M;i++){
         int input1,input2;
         scanf("%d %d",&input1,&input2);
-        // solve(input1,input2);
-        // if(input1 > input2) continue;
         printf("%d\n",dp[input2][input1]);
     }
-
-    // for(auto i=dp.begin();i!=dp.end();i++){
-    //     cout << i->first.first << " -> " << i->first.second << " : " << i->second <<endl;
-    // }
 }
