05b3d070dc652b7f01ea4f8450f6807bdfaafcb9
Dongha Yoon <cs.donghayoon@gmail.com>
2020-09-08 23:54:40 +0900

    0908

--- source/min_max.cpp
+++ source/min_max.cpp
@@ -8,6 +8,8 @@ https://www.acmicpc.net/problem/2357
 
 */
 #include <iostream>
+#include <cmath>
+#include <vector>
 
 #define MAX_VAL = 1'000'000'000
 
@@ -18,46 +20,80 @@ typedef long long int lli;
 struct Node{
     lli max;
     lli min;
+    Node(int a=0, int b=MAX_VAL){ max=a; min=b;}
     void setLeaf(lli N) { max = min = N;}
 };
 
+class SegmentTree{
+    private:
+        Node node_arr*;
+        int height;
+    public:
+        SegmentTree(vector<lli>& vec);
+        ~SegmentTree(){delete [] node_arr;}
+    
+};
 
-Node seg_tree[200000];
+SegmentTree::SegmentTree(vector<lli>& vec){
+    height = (int)log2(vec.size());
+    node_arr = new Node[pow(2,height+1)];
+
+
+
+}
+
 
 int main(){
 
     int N,M;
     cin >> N >> M;
 
-    lli input;
-    for(int i=N;i<2*N;i++){
-        scanf("%lld",&input);
-        seg_tree[i].setLeaf(input);
-    }
+    vector<lli> numset(N);
+
+    for(int i=0;i<N;i++)
+        scanf("%lld",&numset[i]);
+    
+    SegmentTree segTree(numset); //set leaf.
+
+
     for(int i=0;i<M;i++){
         int S,E;
         scanf("%d %d",&S,&E);
         S = N+S-1;
         E = N+E-1;
 
-        Node& start = seg_tree[N+S-1];
-        Node& end = seg_tree[N+E-1];
-        int L = S + S%2;
-        int R = E - (E+1)%2;
+        lli min_v = min(seg_tree[S].min,seg_tree[E].min);
+        lli max_v = max(seg_tree[S].max,seg_tree[E].max);
+        
+        int L = S;// + S%2;
+        int R = E;// - (E+1)%2;
         
         while( L < R && L*R >0){
+            printf("L: %d, R: %d\n",L,R);
+            if(L%2==1){
+                min_v = min(min_v,seg_tree[L].min);
+                max_v = max(max_v,seg_tree[L].max);
+                L = L+1;
+            }
+            if(R%2==0){
+                min_v = min(min_v,seg_tree[R].min);
+                max_v = max(max_v,seg_tree[R].max);
+                R = R-1;
+            }
+
             L = L/2;
             R = R/2;
+            printf("L: %d, R: %d\n",L,R);
             for(int j=L;j<=R;j++){
                 int a,b;
                 a = seg_tree[j].min = min(seg_tree[j*2].min,seg_tree[j*2+1].min);
                 b = seg_tree[j].max = max(seg_tree[j*2].max,seg_tree[j*2+1].max);
                 printf("j: %d- a: %d, b: %d\n",j,a,b);
             }
+            
         }
-        if(L==0) L++;
-        lli min_v = min(start.min,end.min);
-        lli max_v = max(start.max,end.max);
+        // if(L==0) L++;
+        
         
         min_v = min(min_v,seg_tree[L].min);
         max_v = max(max_v,seg_tree[L].max);
